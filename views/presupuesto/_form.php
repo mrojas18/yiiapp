f26a499383a7fe396b7dcf8c5e409832b64c8dfa
manrojas <manrojas@CO20SIST2268A1.ff.favaloro>
2018-08-15 17:27:05 -0300

    se agregaron las liberarias yii jui para  el widget de fechas

--- views/presupuesto/_form.php
+++ views/presupuesto/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\jui\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Presupuesto */
@@ -15,12 +16,12 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'cod_pre')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'id_prov')->textInput() ?>
-    
-    <?= $form->field($model, 'fec_pre')->textInput() ?>
+
+    <?= $form->field($model, 'fec_pre')->widget(DatePicker::className(), ['clientOptions' => ['defaultDate' => '2014-01-01']]) ?>
 
     <?= $form->field($model, 'vencimiento')->textInput() ?>
 
-  
+
 
     <div class="form-group">
         <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
